03a11e67b55e5065cb9890acd9777b8e0a403cfc
Chris Neidhart <cjneidhart@gmail.com>
2020-03-14 23:57:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+const token = fs.readFileSync('token.txt', 'utf8').trim();
+client.login(token);
